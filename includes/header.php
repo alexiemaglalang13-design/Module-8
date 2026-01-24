e94f97d2319ce590277ebecfec035ecd859415b7
alexiemaglalang13-design <alexiemaglalang13@gmail.com>
2026-01-24 15:00:55 +0800

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Flight Schedules</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header class="header">
+    <h1>✈ Flight Schedules</h1>
+    <div class="time-box">
+        <?= date("l, F j, Y | h:i:s A"); ?> (PH)
+    </div>
+</header>
